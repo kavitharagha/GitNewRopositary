90d7f9913192469b8d8aa6cd783d8791ef5fe39d
KavithaRaghav <kavitha.ragha06@gmail.com>
2025-02-05 16:24:23 +0400

    new feature committed successfully

--- src/main/java/com/GitNewRepo/App.java
+++ src/main/java/com/GitNewRepo/App.java
@@ -6,5 +6,8 @@ package com.GitNewRepo;
 public class App {
     public static void main(String[] args) {
         System.out.println("New Project to be stored in Git new repositary!");
+    } 
+    public void display() {
+    	System.out.println("new feature is added");
     }
 }
